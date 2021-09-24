8d53ee374b0378704cee68ac8a222a250f9a79c8
Wilder <wjblah@gmail.com>
2021-09-23 21:42:52 -0500

    organizando el archivo app clase19

--- Programacion-imperartiva-1/clase19/MDT - Métodos de array I/app.js
+++ Programacion-imperartiva-1/clase19/MDT - Métodos de array I/app.js
@@ -17,41 +17,26 @@ let arrayProfesionales = archivos.leerJson("profesionales");
 
 //H
 
-const ordenarPorCosulta = () => {
-    arrayProfesionales.sort((a, b) => {
-        if (a.cantidadConsultas < b.cantidadConsultas) {
-            return 1;
-        }
-        if (a.cantidadConsultas > b.cantidadConsultas) {
-            return -1;
-        }
-        return 0;
-    });
-    return arrayProfesionales;
-};
+const ordenarPorCosulta = (a, b) => b.cantidadConsultas - a.cantidadConsultas;
+console.table(arrayProfesionales.sort(ordenarPorCosulta));
 
-const ordenarPorPuntuacion = () => {
-    arrayProfesionales.sort((a, b) => {
-        if (a.puntuacion < b.puntuacion) {
-            return 1;
-        }
-        if (a.puntuacion > b.puntuacion) {
-            return -1;
-        }
-        return 0;
-    });
-    return arrayProfesionales;
-};
-
-console.log(ordenarPorPuntuacion());
+const ordenarPorPuntuacion = (a, b) => b.puntuacion - a.puntuacion;
+console.table(arrayProfesionales.sort(ordenarPorPuntuacion));
 
 const buscarPorId = (id) => {
     const index = arrayProfesionales.findIndex(
         (arrayProfesionales) => id === arrayProfesionales.identificador
     );
     return index !== -1
-        ? console.log(arrayProfesionales[index])
-        : console.log(`El procional con identificadro ${id} no exite`);
+        ? console.table(arrayProfesionales[index])
+        : console.table(`El procional con identificadro ${id} no exite`);
 };
-buscarPorId(100);
+// buscarPorId(100);
 
+const buscarPorId2 = (id, arr) => {
+    const index = arr.find((arr) => id === arr.identificador);
+    return index !== undefined
+        ? console.table(index)
+        : console.log("El profecional con id " + id + " no existe");
+};
+buscarPorId2(4, arrayProfesionales);
