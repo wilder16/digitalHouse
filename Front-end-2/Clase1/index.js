bcf55ddc9e0f66d2fd295838d953dfd6e8188ee7
Wilder <wjblah@gmail.com>
2022-03-02 19:55:35 -0500

    actualizacion de commit

--- Front-end-2/Clase1/index.js
+++ Front-end-2/Clase1/index.js
@@ -1,7 +1,8 @@
 let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let sum = 0;
 
-for (let i = 0; i < myArray.length; i++) {
-    sum += myArray[i];
-}
+for (let i = 0; i < myArray.length; i++) { sum += myArray[i]; }
+console.log(sum);
+sum = 0;
+myArray.forEach(numero => { sum += numero;});
 console.log(sum);
